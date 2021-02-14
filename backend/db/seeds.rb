a223ed39d3873cb3d050f1ba86969031ed3e9db7
Srog1989 <s.rogers0715@yahoo.com>
2021-02-14 14:29:52 -0700

    configured seed data, user and gift model

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Sarah = User.create(name: "Sarah")
+Jordan = User.create(name: "Jordan")
+
+gift = Sarah.gifts.build(name: "Coat", cost: 75)
+gift.save
+present = Jordan.gifts.build(name: "Fishing Pole", cost: 90)
+present.save
