8d4c5e5fa8609b99e02ac159fb186112741b89d6
Srog1989 <s.rogers0715@yahoo.com>
2021-02-14 16:23:32 -0700

    configured service.js file with api service class

--- frontend/src/service.js
+++ frontend/src/service.js
@@ -1,1 +1,16 @@
 //all fetch functions
+
+class ApiService {
+    constructor(){
+        this.baseUrl = `http://localhost:3000`
+    }
+
+    fetchGifts(){
+        return fetch(`${this.baseUrl}/gifts`, {
+            headers: {
+                'content-type': 'application/json',
+            }
+        })
+        .them(res => res.json())
+    }
+}
