6d30d2581c7d6d21f83b105162dcba7617bd573a
Srog1989 <s.rogers0715@yahoo.com>
2021-02-14 15:35:19 -0700

    modified frontend/gift.js

--- backend/app/controllers/gifts_controller.rb
+++ backend/app/controllers/gifts_controller.rb
@@ -1,3 +1,21 @@
 class GiftsController < ApplicationController
     
+    def index 
+        gifts =Gift.all
+        render json: gifts
+    end 
+
+    def create
+        # user = User.find(params[:user_id])
+        # gift = user.gifts.build({
+        #     nickname:Faker::Name.first_name,
+        #     species:Faker::Games::Pokemon.name
+        # })
+        # render json: pokemon.save ? pokemon : {message: pokemon.errors.messages[:team_max][0]}  
+    end
+
+    def destroy
+        gift = Gift.find(params[:id])
+        gift.destroy
+    end
 end
