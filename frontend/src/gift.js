450b98fd7dc344941abe518e06f99fa4f5e03317
Srog1989 <s.rogers0715@yahoo.com>
2021-02-14 16:12:57 -0700

    modifed gift.js

---
+++ frontend/src/gift.js
@@ -0,0 +1,14 @@
+//class for gif
+// "class(static)" methods and "instance(method on the prototype)" methods for the gift class
+
+
+class Post {
+    static allGifts = []
+
+    constuctor(gift){
+        this.id = gift.id
+        this.name = gift.name
+        this.cost = gift.cost
+        Gift.allGifts.push(this)
+    }
+}
---
+++ frontend/src/index.js
@@ -0,0 +1,5 @@
+//anything that doesnt belong in post.js (or another class file) or service.js
+
+//document.addEventListener('DOMContentLoaded', function(){
+//     console.log("hello")
+// })
