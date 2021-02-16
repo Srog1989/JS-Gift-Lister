fd20523d9c4fb2083b4452d0fcf98a287c73eb7b
Srog1989 <s.rogers0715@yahoo.com>
2021-02-15 19:43:02 -0700

    modified users controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
+
+    def index
+        
+    end
+
+    def create
+
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+    end
     
 end
